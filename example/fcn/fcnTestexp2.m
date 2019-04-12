64e713eeb0ef2f1b644dd988036111516b92d28c
Ah Sue <jonathan.ahsue@gmail.com>
2019-04-12 14:27:55 +0200

    Add example of graph modification in tutorial1 mn

--- example/fcn/fcnTestexp2.m
+++ example/fcn/fcnTestexp2.m
@@ -1,5 +1,7 @@
 function [c] = fcnTestexp2(a,b,d)
 % Second test function
-c = a*b+d;   
+i = fcnTestexp2sub(3);
+disp(i);
+c = a*b+d;
 pause(2);
 end
---
+++ example/fcn/fcnTestexp2sub.m
@@ -0,0 +1,5 @@
+function [c] = fcnTestexp2sub(d)
+% Second test sub-function
+c = d;
+disp('I am in the sub-function of @fcnTestexp2');
+end 
